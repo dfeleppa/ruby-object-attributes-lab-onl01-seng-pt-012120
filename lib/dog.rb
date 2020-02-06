e23c3ba95cf53806426a779a0176e9c694d3ba6f
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-06 11:58:16 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog 
+  def name 
+    @name
+  end
+  
+  def name=()
+    @name
+  end
+end
+
+class Dog
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+end
+ 
+kanye = Person.new("Kanye")
